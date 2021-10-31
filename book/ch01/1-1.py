3c8caed4c5cc5c4d32a78ab19800cf7ce2511a8c
syon0303 <syon03@gmail.com>
2021-10-31 18:13:27 +0900

    그리디 - 거스름돈

--- book/ch01/1-1.py
+++ book/ch01/1-1.py
@@ -1,1 +1,28 @@
-print('hellooo')
+# 나의 풀이
+n = int(input())
+coin_type = [500, 100, 50, 10]
+count = 0
+
+for coin in coin_type:
+    while True:
+        if n//coin == 0:
+            break
+        else:
+            n -= coin
+            count += 1
+print(count)
+
+'''
+정답
+n = int(input())
+count = 0
+
+#큰 단위 화폐부터 차례대로 확인
+coin_types = [500, 100, 50, 10]
+
+for coin in coin_types:
+    count += coin
+    n %= coin
+
+print(count)
+'''
